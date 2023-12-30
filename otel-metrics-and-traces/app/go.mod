c8cad36aa89d1ff96128f0c431a7deab19f0fdc8
taxintt <nthouse14@gmail.com>
2023-12-30 16:01:27 +0900

    restore trace code

--- otel-metrics-and-traces/app/go.mod
+++ otel-metrics-and-traces/app/go.mod
@@ -9,7 +9,6 @@ require (
 	go.opentelemetry.io/otel v1.21.0
 	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v0.44.0
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.21.0
-	go.opentelemetry.io/otel/metric v1.21.0
 	go.opentelemetry.io/otel/sdk v1.21.0
 	go.opentelemetry.io/otel/sdk/metric v1.21.0
 )
@@ -36,6 +35,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
 	go.opentelemetry.io/otel/trace v1.21.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
--- otel-metrics-and-traces/app/main.go
+++ otel-metrics-and-traces/app/main.go
@@ -10,11 +10,15 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.opentelemetry.io/contrib/detectors/gcp"
 	"go.opentelemetry.io/otel"
 
+	texporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 )
 
@@ -60,9 +64,9 @@ func main() {
 	}
 
 	// create tracer
-	// traceProvider := newTraceProvider(ctx)
-	// tracer := traceProvider.Tracer("github.com/taxintt/otel-traces-demo")
-	// otel.SetTracerProvider(traceProvider)
+	traceProvider := newTraceProvider(ctx)
+	tracer := traceProvider.Tracer("github.com/taxintt/otel-traces-demo")
+	otel.SetTracerProvider(traceProvider)
 
 	// create middleware
 	e.Use(middleware.Logger())
@@ -70,8 +74,8 @@ func main() {
 
 	e.GET("/", func(c echo.Context) error {
 		// create span
-		// _, span := tracer.Start(ctx, "op1")
-		// defer span.End()
+		_, span := tracer.Start(ctx, "op1")
+		defer span.End()
 
 		time.Sleep(1000 * time.Millisecond)
 
@@ -88,42 +92,42 @@ func main() {
 	<-ctx.Done()
 }
 
-// func newTraceProvider(ctx context.Context) *sdktrace.TracerProvider {
-// 	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+func newTraceProvider(ctx context.Context) *sdktrace.TracerProvider {
+	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
 
-// 	var exporter sdktrace.SpanExporter
-// 	var err error
+	var exporter sdktrace.SpanExporter
+	var err error
 
-// 	if isCloudRun := os.Getenv("K_SERVICE") != ""; isCloudRun {
-// 		exporter, err = texporter.New(texporter.WithProjectID(projectID))
-// 		if err != nil {
-// 			log.Fatalf("texporter.New: %v", err)
-// 		}
-// 	} else {
-// 		exporter, err = stdouttrace.New(stdouttrace.WithPrettyPrint())
-// 		if err != nil {
-// 			log.Fatalf("stdouttrace.New: %v", err)
-// 		}
-// 	}
+	if isCloudRun := os.Getenv("K_SERVICE") != ""; isCloudRun {
+		exporter, err = texporter.New(texporter.WithProjectID(projectID))
+		if err != nil {
+			log.Fatalf("texporter.New: %v", err)
+		}
+	} else {
+		exporter, err = stdouttrace.New(stdouttrace.WithPrettyPrint())
+		if err != nil {
+			log.Fatalf("stdouttrace.New: %v", err)
+		}
+	}
 
-// 	res, err := resource.New(ctx,
-// 		// Use the GCP resource detector to detect information about the GCP platform
-// 		resource.WithDetectors(gcp.NewDetector()),
-// 		// Keep the default detectors
-// 		resource.WithTelemetrySDK(),
-// 		// Add your own custom attributes to identify your application
-// 		resource.WithAttributes(
-// 			semconv.ServiceNameKey.String("sample-local-app"),
-// 		),
-// 	)
-// 	if err != nil {
-// 		log.Fatalf("resource.New: %v", err)
-// 	}
-// 	provider := sdktrace.NewTracerProvider(
-// 		sdktrace.WithBatcher(exporter),
-// 		sdktrace.WithResource(res),
-// 	)
-// 	defer provider.Shutdown(ctx) // flushes any pending spans, and closes connections.
+	res, err := resource.New(ctx,
+		// Use the GCP resource detector to detect information about the GCP platform
+		resource.WithDetectors(gcp.NewDetector()),
+		// Keep the default detectors
+		resource.WithTelemetrySDK(),
+		// Add your own custom attributes to identify your application
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String("sample-local-app"),
+		),
+	)
+	if err != nil {
+		log.Fatalf("resource.New: %v", err)
+	}
+	provider := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(res),
+	)
+	defer provider.Shutdown(ctx) // flushes any pending spans, and closes connections.
 
-// 	return provider
-// }
+	return provider
+}

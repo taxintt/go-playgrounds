cb4fe56d7a6771259369b43945a0066473911bf6
taxintt <nthouse14@gmail.com>
2024-01-03 16:08:26 +0900

    modify to run local with docker-compose

--- ent-with-atlas/main.go
+++ ent-with-atlas/main.go
@@ -2,21 +2,52 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/taxintt/go-playgrounds/ent-with-atlas/ent"
+	"github.com/taxintt/go-playgrounds/ent-with-atlas/ent/user"
 
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	client, err := ent.Open("postgres", "host=localhost port=5432 user=your_username dbname=your_database password=your_password")
+	client, err := ent.Open("postgres", "host=postgres port=5432 user=postgres dbname=testdb password=postgres sslmode=disable")
 	if err != nil {
 		log.Fatalf("failed opening connection to postgres: %v", err)
 	}
 	defer client.Close()
+
 	// Run the auto migration tool.
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
+
+}
+
+func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", u)
+	return u, nil
+}
+
+func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Query().
+		Where(user.Name("a8m")).
+		// `Only` fails if no user found,
+		// or more than 1 user returned.
+		Only(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed querying user: %w", err)
+	}
+	log.Println("user returned: ", u)
+	return u, nil
 }

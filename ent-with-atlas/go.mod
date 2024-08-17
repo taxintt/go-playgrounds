42e4dc5b62fdd77d290f695e5fb77eba92644672
taxintt <nthouse14@gmail.com>
2024-08-17 15:10:54 +0900

    implement http server (create user and query user info

--- ent-with-atlas/go.mod
+++ ent-with-atlas/go.mod
@@ -4,7 +4,9 @@ go 1.21
 
 require (
 	entgo.io/ent v0.12.5
+	github.com/go-chi/chi v1.5.5
 	github.com/lib/pq v1.10.9
+	go.uber.org/zap v1.27.0
 )
 
 require (
@@ -17,6 +19,7 @@ require (
 	github.com/hashicorp/hcl/v2 v2.13.0 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/zclconf/go-cty v1.8.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 )
--- ent-with-atlas/main.go
+++ ent-with-atlas/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"strconv"
 
+	"github.com/go-chi/chi"
 	"github.com/taxintt/go-playgrounds/ent-with-atlas/ent"
 	"github.com/taxintt/go-playgrounds/ent-with-atlas/ent/user"
 
 	_ "github.com/lib/pq"
 )
+
+type server struct {
+	client *ent.Client
+}
+
+func newServer(client *ent.Client) *server {
+	return &server{client: client}
+}
 
 func main() {
 	client, err := ent.Open("postgres", "host=postgres port=5432 user=postgres dbname=testdb password=postgres sslmode=disable")
@@ -19,35 +32,51 @@ func main() {
 	defer client.Close()
 
 	// Run the auto migration tool.
+	// https://entgo.io/ja/docs/getting-started/#automatic-migrations
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
 
+	srv := newServer(client)
+	r := chi.NewRouter()
+
+	r.Route("/user", func(r chi.Router) {
+		r.Get("/{userID}", srv.queryUser)
+		r.Post("/", srv.createUser)
+	})
+
+	fmt.Println("Server running")
+	defer fmt.Println("Server stopped")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
-	u, err := client.User.
-		Create().
-		SetAge(30).
-		SetName("a8m").
-		Save(ctx)
+func (s *server) queryUser(w http.ResponseWriter, r *http.Request) {
+	userIDString := chi.URLParam(r, "userID")
+	userID, err := strconv.Atoi(userIDString)
 	if err != nil {
-		return nil, fmt.Errorf("failed creating user: %w", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
-	log.Println("user was created: ", u)
-	return u, nil
+	u, err := s.client.User.Query().Where(user.ID(userID)).Only(r.Context())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "user returned: %v", u)
 }
 
-func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
-	u, err := client.User.
-		Query().
-		Where(user.Name("a8m")).
-		// `Only` fails if no user found,
-		// or more than 1 user returned.
-		Only(ctx)
+func (s *server) createUser(w http.ResponseWriter, r *http.Request) {
+	body, _ := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	var user ent.User
+	json.Unmarshal(body, &user)
+	u, err := s.client.User.Create().SetAge(user.Age).SetName(user.Name).Save(r.Context())
 	if err != nil {
-		return nil, fmt.Errorf("failed querying user: %w", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	log.Println("user returned: ", u)
-	return u, nil
+	fmt.Fprintf(w, "user was created: %v", u)
 }

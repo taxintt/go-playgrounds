722b0e58dcf439135ff2a73b6afdb18ca7478bfd
taxintt <nthouse14@gmail.com>
2025-01-01 13:33:03 +0900

    setup sqlite3 sample code which fetchs Chrome history

---
+++ sqlite/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./history.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+	select substr(title, 1, $cols) as title, url
+  from urls order by last_visit_time desc;
+	`
+
+	rows, err := db.Query(sqlStmt, 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var title, url string
+		err = rows.Scan(&title, &url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(title, " | ", url)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
